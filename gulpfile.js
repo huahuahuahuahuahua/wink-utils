bf32c2b0a6193a176f6b4dca81556767a8849b83
huawink <1017739427@qq.com>
2022-04-17 15:47:33 +0800

    1.0.4

--- gulpfile.js
+++ gulpfile.js
@@ -399,10 +399,7 @@ const taskUpdateVersion = () => {
 const taskPublish = async (done) => {
   // 升级版本号
   console.log("--------------------开发发布包到npm");
-  const versionFlag = await sh(
-    "npm publish",
-    path.join(__dirname, "..", "build")
-  );
+  const versionFlag = await sh("npm publish");
   if (versionFlag.code === 1) {
     console.log("------------------发布版本成功");
     resolve();
